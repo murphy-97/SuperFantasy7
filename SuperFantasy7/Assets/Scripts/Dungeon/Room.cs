0cbf4365fbb1373579ceb386f0ca163b79baabe4
murphy-97 <dmurphy@cs.nmt.edu>
2020-04-15 12:15:01 -0600

    Added basic room script

---
+++ SuperFantasy7/Assets/Scripts/Dungeon/Room.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Room : MonoBehaviour
+{
+    // Class (static) properties
+
+    // Class (static) methods
+
+    // Unity methods
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    // Object properties
+    [SerializeField] private GameObject door_n;
+    [SerializeField] private GameObject door_s;
+    [SerializeField] private GameObject door_e;
+    [SerializeField] private GameObject door_w;
+
+    // Object methods
+}
