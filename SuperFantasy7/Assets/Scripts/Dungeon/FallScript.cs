bfb9df6b99777b707157d42a12cb6f38f90e2a23
ImaginationWaterfall <ImaginationWaterfall@users.noreply.github.com>
2020-04-19 01:43:11 -0600

    Created Rooms and obstacles
    Created 5  dungeon rooms and created some obstacles. Currently, the only obstacles are spikes and unstable floors.

---
+++ SuperFantasy7/Assets/Scripts/Dungeon/FallScript.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FallScript : MonoBehaviour
+{
+
+    public float destroyDelay;
+
+    void OnCollisionEnter(Collision collision)
+    {
+        if(collision.gameObject.name == "Player")
+        {
+            StartCoroutine(DestroyDelay());
+        }
+    }
+
+    IEnumerator DestroyDelay()
+    {
+        yield return new WaitForSeconds(destroyDelay);
+        Destroy(gameObject);
+    }
+
+
+}
---
+++ SuperFantasy7/Assets/Scripts/Dungeon/Spike.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Spike : MonoBehaviour
+{
+    public int damge;
+    public int pushBack;
+
+    void OnCollisionEnter(Collision collision)
+    {
+        if(collision.gameObject.name == "Player")
+        {
+            Vector3 enterDirection = collision.GetContact(0).point - transform.position;
+
+            enterDirection.Normalize();
+
+            enterDirection.z = 0;
+
+            collision.gameObject.GetComponent<Rigidbody>().AddForce(enterDirection*pushBack);
+        }
+    }
+}
