5e9ff807733d1a5db4318e6938a1e3c76bd16f6a
ImaginationWaterfall <ImaginationWaterfall@users.noreply.github.com>
2020-04-30 00:31:34 -0600

    Completed item spawn room, changed hook shot position in unstable floor room

--- SuperFantasy7/Assets/Scripts/Dungeon/SpawnItem.cs
+++ SuperFantasy7/Assets/Scripts/Dungeon/SpawnItem.cs
@@ -5,16 +5,58 @@ using UnityEngine;
 public class SpawnItem : MonoBehaviour
 {
     public Transform spawn;
-    
+    public Transform spawnPoint;
+    public Transform endPoint;
+    public float fallSpeed;
+    public float spawnDelay;
+
+    private GameObject item;
+    private bool falling;
+    private bool waiting;
+    private float length;
+    private float startTime;
     // Start is called before the first frame update
     void Start()
     {
-        
+        item = Instantiate(spawn, spawnPoint.position, spawnPoint.rotation).gameObject;
+        falling = true;
+        waiting = false;
+        length = Vector3.Distance(spawnPoint.position, endPoint.position);
+        startTime = Time.time;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if(item == null)
+        {
+            if (!waiting)
+            {
+                StartCoroutine(delay());
+            }
+        }
+
+        if (falling && item != null)
+        {
+            float distanceCovered = (Time.time - startTime) * fallSpeed;
+
+            item.transform.position = Vector3.Lerp(spawnPoint.position, endPoint.position, distanceCovered/length);
+
+            if (Vector3.Distance(item.transform.position, endPoint.position) <= 0.001f)
+            {
+                falling = false;
+            }
+        }
+    }
+
+    public IEnumerator delay()
+    {
+        waiting = true;
+        yield return new WaitForSeconds(spawnDelay);
+        item = Instantiate(spawn, spawnPoint.position, spawnPoint.rotation).gameObject;
+        falling = true;
+        waiting = false;
+        length = Vector3.Distance(spawnPoint.position, endPoint.position);
+        startTime = Time.time;
     }
 }
